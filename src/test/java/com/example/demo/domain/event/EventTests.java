e780c8245eb09b983cc6b04acb403c40553be4b0
Neslihan Avsar <neslihan.avsar@edu.tbz.ch>
2025-02-20 16:57:09 +0100

    defined test cases for events

---
+++ src/test/java/com/example/demo/domain/event/EventTests.java
@@ -0,0 +1,91 @@
+package com.example.demo.domain.event;
+
+import org.junit.jupiter.api.Test;
+
+public class EventTests {
+
+    // testing GET /event
+    @Test
+    void getAllEvents_Successful() {
+        
+    }
+
+    @Test
+    void getAllEvents_FailedDueToNoAuthentication() {
+
+    }
+
+    // testing GET /event/id
+    @Test
+    void getEvent_Successful() {
+
+    }
+
+    @Test
+    void getEvent_FailedDueToNoAuthentication() {
+
+    }
+
+    @Test
+    void getEvent_FailedDueToInvalidInput() {
+
+    }
+
+    // testing POST /event
+    @Test
+    void createEvent_Successful() {
+
+    }
+
+    @Test
+    void createEvent_FailedDueToNoAuthentication() {
+
+    }
+
+    @Test
+    void createEvent_FailedDueToInvalidInput() {
+
+    }
+
+    // testing PUT /event/id
+    @Test
+    void editEvent_Successful() {
+
+    }
+
+    @Test
+    void editEvent_FailedDueToNoAuthentication() {
+
+    }
+
+    @Test
+    void editEvent_FailedDueToInvalidInput() {
+
+    }
+
+    @Test
+    void editEvent_FailedDueToMissingAuthorization() {
+
+    }
+
+    // testing DELETE /event/id
+    @Test
+    void deleteEvent_Successful() {
+
+    }
+
+    @Test
+    void deleteEvent_FailedDueToNoAuthentication() {
+
+    }
+
+    @Test
+    void deleteEvent_FailedDueToInvalidInput() {
+
+    }
+
+    @Test
+    void deleteEvent_FailedDueToMissingAuthorization() {
+
+    }
+}
